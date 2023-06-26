51ff97fa23f99c73ee9b71b509e873b35487c2d1
firro2 <117009530+firro2@users.noreply.github.com>
2023-06-26 18:47:38 +0300

    Add files via upload

---
+++ C32 script.js
@@ -0,0 +1,15 @@
+function checkPassword() {
+    const password = document.getElementById("password").value;
+    if (password === "Блестящий") {
+      window.location.href="C32 3.html";
+      return false;
+    } else if (password === "7892")  {
+    
+        window.location.href="C32 2.html";
+        return false;
+    }else{
+        alert("ERROR")
+    }
+    }
+
+
